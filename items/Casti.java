ed5fcfd02624249ae89edd83461d753ab927f4d3
golddy-star <aurelian.sumurduc@gmail.com>
2025-11-09 20:16:14 +0200

    Update Casti.java

--- items/Casti.java
+++ items/Casti.java
@@ -2,11 +2,11 @@ package items;
 import java.util.ArrayList;
 
 public class Casti extends Gadget {
-    boolean microfon;
-    boolean true_wireless;
-    String tip;
-    int timp_incarcare;
-    int raspuns_in_frecventa;
+    private boolean microfon;
+    private boolean true_wireless;
+    private String tip;
+    private int timp_incarcare;
+    private int raspuns_in_frecventa;
     public Casti() {
        super();
        this.microfon = false;
@@ -60,87 +60,6 @@ public class Casti extends Gadget {
                 ", raspuns_in_frecventa= " + raspuns_in_frecventa + '\'' +'}';
 
     }
-    public static ArrayList<Casti> cauta1(ArrayList<Casti> lista, int optiune, String valoare) {
-        ArrayList<Casti> rezultate = new ArrayList<>();
-
-        for (Casti a : lista) {
-            switch (optiune) {
-                case 1:
-                    if (a.getMicrofon() == Boolean.parseBoolean(valoare)) {
-                        rezultate.add(a);
-                    }
-                    break;
-                case 2:
-                    if (a.getTrue_wireless() == Boolean.parseBoolean(valoare)) {
-                        rezultate.add(a);
-                    }
-                    break;
-                case 3:
-                    if (a.getTip().equalsIgnoreCase(valoare)) {
-                        rezultate.add(a);
-                    }
-                    break;
-                case 4:
-                    if (a.getTimp_incarcare() == Integer.parseInt(valoare)) {
-                        rezultate.add(a);
-                    }
-                    break;
-                case 5:
-                    if (a.getRaspuns_in_frecventa() == Integer.parseInt(valoare)) {
-                        rezultate.add(a);
-                    }
-                    break;
-                default:
-                    System.out.println("Produs invalid");
-            }
-        }
-        if (rezultate.isEmpty()) {
-            System.out.println("Niciun produs găsit pentru valoarea: " + valoare);
-        }
-        return rezultate;
-    }
-
-    public static void cauta(ArrayList<Casti> lista, int optiune, String valoare) {
-        boolean gasit = false;
-        for (Casti a : lista) {
-            switch (optiune) {
-                case 1:
-                    if (a.getMicrofon() == Boolean.parseBoolean(valoare)) {
-                        System.out.println(a);
-                        gasit = true;
-                    }
-                    break;
-                case 2:
-                    if (a.getTrue_wireless() == Boolean.parseBoolean(valoare)) {
-                        System.out.println(a);
-                        gasit = true;
-                    }
-                    break;
-                case 3:
-                    if (a.getTip().equalsIgnoreCase(valoare)) {
-                        System.out.println(a);
-                        gasit = true;
-                    }
-                    break;
-                case 4:
-                    if (a.getTimp_incarcare() == Integer.parseInt(valoare)) {
-                        System.out.println(a);
-                        gasit = true;
-                    }
-                    break;
-                case 5:
-                    if (a.getRaspuns_in_frecventa() == Integer.parseInt(valoare)) {
-                        System.out.println(a);
-                        gasit = true;
-                    }
-                    break;
-                default:
-                    System.out.println("Produs invalid");
-            }
-        }
-        if(!gasit) {
-            System.out.println("Produs gasit");
-        }
-    }
 
 }
+
