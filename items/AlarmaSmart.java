21fc92aa1e9f72ef5660bbc81a1e9658ad7037c3
Alex0055 <49958009+Alex0055@users.noreply.github.com>
2025-11-09 21:14:36 +0300

    Add files via upload

---
+++ items/AlarmaSmart.java
@@ -0,0 +1,78 @@
+package items;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class AlarmaSmart extends Gadget {
+    private String tip;
+    private int nivelSunet;
+    private int tensiune;
+    private String tipEcran;
+    private int greutate;
+
+    //fara parametri
+    public AlarmaSmart() {
+        super();
+        this.tip = "Tip";
+        this.nivelSunet = 0;
+        this.tensiune = 0;
+        this.tipEcran = "LCD";
+        this.greutate = 0;
+    }
+
+    //cu parametri
+    public AlarmaSmart(String nume, String culoare, float pret, int autonomie, String interfata, int stoc,
+                       String tip, int nivelSunet, int tensiune, String tipEcran ,int greutate) {
+        super(nume, culoare, pret, autonomie, interfata, stoc);
+        this.tip = tip;
+        this.nivelSunet = nivelSunet;
+        this.tensiune = tensiune;
+        this.tipEcran = tipEcran;
+        this.greutate = greutate;
+    }
+
+    //copiere
+    public AlarmaSmart(AlarmaSmart other) {
+        super(other);
+        this.tip = other.tip;
+        this.nivelSunet = other.nivelSunet;
+        this.tensiune = other.tensiune;
+        this.tipEcran = other.tipEcran;
+        this.greutate = other.greutate;
+    }
+
+    //get
+    public String getTip(){ return this.tip; }
+    public int getNivelSunet() { return this.nivelSunet; }
+    public int getTensiune() { return this.tensiune; }
+    public String getTipEcran() {  return this.tipEcran; }
+    public int getGreutate() { return this.greutate; }
+
+    //set
+    public void setTip(String tip){this.tip = tip;}
+    public void setNivelSunet(int nivelSunet) {this.nivelSunet = nivelSunet;}
+    public void setTensiune(int tensiune) {this.tensiune = tensiune;}
+    public void setTipEcran(String tipEcran) {this.tipEcran = tipEcran;}
+    public void setGreutate(int greutate) {this.greutate = greutate;}
+
+
+
+    //to string
+    @Override
+    public String toString() {
+        return "AlarmaSmart object{" +
+                "nume='" + nume + '\'' +
+                "culoare='" + culoare + '\'' +
+                "pret='" + pret + '\'' +
+                "autonomie='" + autonomie + '\'' +
+                "interfata='" + interfata + '\'' +
+                "stoc='" + stoc + '\'' +
+                "tip='" + tip + '\'' +
+                ", nivelSunet=" + nivelSunet + '\''+
+                ", tensiune=" + tensiune +  '\''+
+                ", tipEcran='" + tipEcran + '\'' +
+                ", greutate=" + greutate +  '\''+
+                '}';
+    }
+    }
+
+
---
+++ items/PrizaSmart.java
@@ -0,0 +1,76 @@
+package items;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class PrizaSmart extends Gadget  {
+    private String utilizare;
+    private boolean smart;
+    private String material;
+    private int amperaj;
+    private int tensiune;
+
+    //fara
+    public PrizaSmart() {
+        super();
+        this.utilizare = "Casa";
+        this.smart = true;
+        this.material = "Plastic";
+        this.amperaj = 0;
+        this.tensiune = 0;
+    }
+
+    //cu
+    public PrizaSmart(String nume, String culoare, float pret, int autonomie, String interfata, int stoc,
+                      String utilizare, boolean smart, String material, int amperaj, int tensiune) {
+        super(nume, culoare, pret, autonomie, interfata, stoc);
+        this.utilizare = utilizare;
+        this.smart = smart;
+        this.material = material;
+        this.amperaj = amperaj;
+        this.tensiune = tensiune;
+    }
+
+    //copi
+    public PrizaSmart(PrizaSmart other) {
+        super(other);
+        this.utilizare = other.utilizare;
+        this.smart = other.smart;
+        this.material = other.material;
+        this.amperaj = other.amperaj;
+        this.tensiune = other.tensiune;
+    }
+
+    //get
+    public String getUtilizare(){ return this.utilizare; }
+    public boolean getSmart() { return this.smart; }
+    public String getMaterial(){ return this.material; }
+    public int getAmperaj(){ return this.amperaj; }
+    public int getTensiune(){ return this.tensiune; }
+
+
+
+    public void setUtilizare(String utilizare){this.utilizare = utilizare;}
+    public void setSmart(boolean smart){this.smart = smart;}
+    public void setMaterial(String material){this.material = material;}
+    public void setAmperaj(int amperaj){this.amperaj = amperaj;}
+    public void setTensiune(int tensiune){this.tensiune = tensiune;}
+
+
+    @Override
+    public String toString() {
+        return "PrizaSmart object{" +
+                "nume='" + nume + '\'' +
+                "culoare='" + culoare + '\'' +
+                "pret='" + pret + '\'' +
+                "autonomie='" + autonomie + '\'' +
+                "interfata='" + interfata + '\'' +
+                "stoc='" + stoc + '\'' +
+                "utilizare='" + utilizare + '\'' +
+                ", smart=" + smart +    '\''+
+                ", material='" + material + '\'' +
+                ", amperaj=" + amperaj +    '\''+
+                ", tensiune=" + tensiune +  '\''+
+                '}';
+    }
+
+}
