d77db862f9942fc041a346e9799ca5b4617455a5
DragneaCristian13 <dragnearomeo1234@gmail.com>
2025-11-09 20:16:09 +0200

    Add files via upload

---
+++ items/MiniCameraVideo.java
@@ -0,0 +1,81 @@
+package items;
+
+public class MiniCameraVideo extends Gadget {
+    public int MemorieInterna;
+    public String Rezolutie;
+    public int MemorieExterna;
+    public int CadrePeSecunda;
+    public String Dimensiuni;
+    public MiniCameraVideo() {
+        super();
+        this.MemorieInterna = 0;
+        this.Rezolutie = "";
+        this.MemorieExterna = 0;
+        this.CadrePeSecunda = 0;
+        this.Dimensiuni = "";
+    }
+    public MiniCameraVideo(String nume,String culoare,float pret,int autonomie,String interfata,int stoc,int MemorieInterna,String Rezolutie,int MemorieExterna,int CadrePeSecunda,String Dimensiuni) {
+        super(nume,culoare,pret,autonomie,interfata,stoc );
+        this.MemorieInterna=MemorieInterna;
+        this.Rezolutie=Rezolutie;
+        this.MemorieExterna=MemorieExterna;
+        this.CadrePeSecunda=CadrePeSecunda;
+        this.Dimensiuni=Dimensiuni;
+
+    }
+    public MiniCameraVideo(MiniCameraVideo minicamera) {
+        super(minicamera);
+        this.MemorieExterna=minicamera.MemorieExterna;
+        this.Rezolutie=minicamera.Rezolutie;
+        this.MemorieInterna=minicamera.MemorieInterna;
+        this.CadrePeSecunda=minicamera.CadrePeSecunda;
+        this.Dimensiuni=minicamera.Dimensiuni;
+    }
+
+    @Override
+    public String toString() {
+        return "Gadget object {" +
+                "nume= " + nume + '\'' +
+                ", culoare= " + culoare + '\'' +
+                ", pret= " + pret + '\'' +
+                ", autonomie= " + autonomie + '\'' +
+                ", interfata= " + interfata + '\'' +
+                ", MemorieInterna= " + MemorieInterna + '\'' +
+                ", MemorieExterna= " + MemorieExterna + '\'' +
+                ", CadrePeSecunda= " + CadrePeSecunda + '\'' +
+                ", Dimensiuni= " + Dimensiuni + '\'' +
+                ", Rezolutie= " + Rezolutie + '\'' +
+                ", stoc= " + stoc + '}';
+    }
+    public void setMemorieInterna(int MemorieInterna) {
+        this.MemorieInterna = MemorieInterna;
+    }
+    public void setRezolutie(String Rezolutie) {
+        this.Rezolutie = Rezolutie;
+    }
+    public void setMemorieExterna(int MemorieExterna) {
+        this.MemorieExterna = MemorieExterna;
+    }
+    public void setCadrePeSecunda(int CadrePeSecunda) {
+        this.CadrePeSecunda = CadrePeSecunda;
+    }
+    public void setDimensiuni(String Dimensiuni) {
+        this.Dimensiuni = Dimensiuni;
+    }
+
+    public int getMemorieInterna() {
+        return MemorieInterna;
+    }
+    public String getRezolutie() {
+        return Rezolutie;
+    }
+    public int getMemorieExterna() {
+        return MemorieExterna;
+    }
+    public int getCadrePeSecunda() {
+        return CadrePeSecunda;
+    }
+    public String getDimensiuni() {
+        return Dimensiuni;
+    }
+}
---
+++ items/Reportofon.java
@@ -0,0 +1,81 @@
+package items;
+
+public class Reportofon extends Gadget{
+    public int MemorieInterna;
+    public int MemorieExterna;
+    public String Dimensiuni;
+    public String CalitateSunet;
+    public int NumarMicrofoane;
+    public Reportofon(){
+        super();
+        this.MemorieInterna = 0;
+        this.MemorieExterna = 0;
+        this.Dimensiuni = "";
+        this.CalitateSunet = "";
+        this.NumarMicrofoane = 0;
+
+    }
+    public Reportofon(String nume,String culoare,float pret,int autonomie,String interfata,int stoc,int MemorieInterna,int MemorieExterna,int CadrePeSecunda,String Dimensiuni,int NumarMicrofoane) {
+        super(nume,culoare,pret,autonomie,interfata,stoc );
+        this.MemorieInterna = MemorieInterna;
+        this.MemorieExterna = MemorieExterna;
+        this.Dimensiuni = Dimensiuni;
+        this.CalitateSunet = CalitateSunet;
+        this.NumarMicrofoane = NumarMicrofoane;
+    }
+    public Reportofon(Reportofon reportofon){
+        super(reportofon);
+        this.MemorieInterna= reportofon.MemorieInterna;
+        this.MemorieExterna= reportofon.MemorieExterna;
+        this.Dimensiuni= reportofon.Dimensiuni;
+        this.CalitateSunet= reportofon.CalitateSunet;
+        this.NumarMicrofoane= reportofon.NumarMicrofoane;
+
+    }
+    @Override
+    public String toString() {
+        return "Gadget object {" +
+                "nume= " + nume + '\'' +
+                ", culoare= " + culoare + '\'' +
+                ", pret= " + pret + '\'' +
+                ", autonomie= " + autonomie + '\'' +
+                ", interfata= " + interfata + '\'' +
+                ", stoc= " + stoc + '\'' +
+                ", MemorieInterna= " + MemorieInterna + '\'' +
+                ", MemorieExterna= " + MemorieExterna + '\'' +
+                ", NumarMicrofoane= " + NumarMicrofoane + '\'' +
+                ", CalitateSunet= " + CalitateSunet + '\'' +
+                ", Dimensiuni= " + Dimensiuni + '\'' + '}';
+
+    }
+    public void setMemorieInterna(int MemorieInterna) {
+        this.MemorieInterna = MemorieInterna;
+    }
+    public void setMemorieExterna(int MemorieExterna) {
+        this.MemorieExterna = MemorieExterna;
+    }
+    public void setDimensiuni(String Dimensiuni) {
+        this.Dimensiuni = Dimensiuni;
+    }
+    public void setCalitateSunet(String CalitateSunet) {
+        this.CalitateSunet = CalitateSunet;
+    }
+    public void setNumarMicrofoane(int NumarMicrofoane) {
+        this.NumarMicrofoane = NumarMicrofoane;
+    }
+    public int getMemorieInterna() {
+        return MemorieInterna;
+    }
+    public int getMemorieExterna() {
+        return MemorieExterna;
+    }
+    public String getDimensiuni() {
+        return Dimensiuni;
+    }
+    public String getCalitateSunet() {
+        return CalitateSunet;
+    }
+    public int getNumarMicrofoane() {
+        return NumarMicrofoane;
+    }
+}
