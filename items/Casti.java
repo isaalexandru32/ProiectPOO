fa34be4decb369c935d185bce5451d2ac0f84998
golddy-star <aurelian.sumurduc@gmail.com>
2025-11-23 17:21:30 +0200

    Casti.java

--- items/Casti.java
+++ items/Casti.java
@@ -61,23 +61,83 @@ public class Casti extends Gadget {
 
     }
 
-    public static ArrayList<Casti> cautareTrueWireless(ArrayList<Casti> lista, boolean microfon, boolean true_wireless) {
-        ArrayList<Casti> cautare = new ArrayList<Casti>();
-        for (Casti casti : lista) {
-            if(casti.microfon == microfon && casti.true_wireless == true_wireless ) {
-                cautare.add(casti);
+    public static ArrayList<Casti> cautaMicrofon(ArrayList<Casti> listaCasti, String valoare) {
+        ArrayList<Casti> rezultat = new ArrayList<>();
+        boolean cautaMicrofon = Boolean.parseBoolean(valoare);
+        for (Casti a : listaCasti) {
+            if (a.getMicrofon() == cautaMicrofon) {
+                rezultat.add(a);
             }
         }
-        return cautare;
-    }
-    public static ArrayList<Casti> cautareTimpIncarcare(ArrayList<Casti> lista, int timp_incarcare, int raspuns_in_frecventa) {
-        ArrayList<Casti> cautare = new ArrayList<Casti>();
-        for (Casti casti : lista) {
-            if(casti.timp_incarcare == timp_incarcare && casti.raspuns_in_frecventa == raspuns_in_frecventa ) {
-                cautare.add(casti);
-            }
-        }
-        return cautare;
+        return rezultat;
     }
 
+    public static ArrayList<Casti> cautaTrueWireless(ArrayList<Casti> listaCasti, String valoare) {
+        ArrayList<Casti> rezultat = new ArrayList<>();
+        boolean cautaTrueWireless = Boolean.parseBoolean(valoare);
+        for (Casti a : listaCasti) {
+            if (a.getTrue_wireless() == cautaTrueWireless) {
+                rezultat.add(a);
+            }
+        }
+        return rezultat;
+    }
+
+
+    public static ArrayList<Casti> cautaTip(ArrayList<Casti> listaCasti, String valoare) {
+        ArrayList<Casti> rezultat = new ArrayList<>();
+        for (Casti a : listaCasti) {
+            if (a.getTip().equalsIgnoreCase(valoare)){
+                rezultat.add(a);
+            }
+        }
+        return rezultat;
+    }
+
+    public static ArrayList<Casti> cautaTimpIncarcare(ArrayList<Casti> listaCasti, String valoare) {
+        ArrayList<Casti> rezultat = new ArrayList<>();
+        for (Casti a : listaCasti) {
+            if (a.getTimp_incarcare() == Integer.parseInt(valoare)){
+                rezultat.add(a);
+            }
+        }
+        return rezultat;
+    }
+
+    public static ArrayList<Casti> cautaRaspunsInFrecventa(ArrayList<Casti> listaCasti,  String valoare) {
+        ArrayList<Casti> rezultat = new ArrayList<>();
+        for (Casti a : listaCasti) {
+            if (a.getRaspuns_in_frecventa() == Integer.parseInt(valoare)){
+                rezultat.add(a);
+            }
+        }
+        return rezultat;
+    }
+
+    public static ArrayList<Casti> cautaPret(ArrayList<Casti> listaCasti,  String valoare) {
+        ArrayList<Casti> rezultat = new ArrayList<>();
+        for (Casti a : listaCasti) {
+            if (a.getPret() == Float.parseFloat(valoare)){
+                rezultat.add(a);
+            }
+        }
+        return rezultat;
+    }
+
+
+    public static ArrayList<Casti> cautaCasti(ArrayList<Casti> listaCasti, int optiune, String valoare){
+
+        switch(optiune){
+            case 1: return cautaMicrofon(listaCasti, valoare);
+            case 2: return cautaTrueWireless(listaCasti, valoare);
+            case 3: return cautaTip(listaCasti, valoare);
+            case 4: return cautaTimpIncarcare(listaCasti, valoare);
+            case 5: return cautaRaspunsInFrecventa(listaCasti, valoare);
+            case 6: return cautaPret(listaCasti, valoare);
+
+            default:
+                System.out.println("Optiune invalida!");
+                return new ArrayList<>();
+        }
+    }
 }
