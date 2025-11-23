35be3803234cdef68d4f2bb47f4ae111a62461ad
DragneaCristian13 <dragnearomeo1234@gmail.com>
2025-11-23 17:36:33 +0200

    Update Reportofon.java

--- items/Reportofon.java
+++ items/Reportofon.java
@@ -77,27 +77,88 @@ public class Reportofon extends Gadget{
         return NumarMicrofoane;
     }
 
-    public static ArrayList<Reportofon> cautaDimensiuniMemorieExterna(ArrayList<Reportofon> listaReportofon, String Dimensiuni, int MemorieExterna) {
-        ArrayList<Reportofon> gasit = new ArrayList<>();
-
-        for (Reportofon rep : listaReportofon) {
-            if (rep.Dimensiuni == Dimensiuni && rep.MemorieExterna== MemorieExterna) {
-                gasit.add(rep);
+    public static ArrayList<Reportofon> cautaMemorieInterna(ArrayList<Reportofon> listaReportofon, String valoare) {
+            ArrayList<Reportofon> rezultat = new ArrayList<>();
+            for (Reportofon a : listaReportofon) {
+                if (a.getMemorieInterna() == Integer.parseInt(valoare)){
+                    rezultat.add(a);
+                }
             }
+            return rezultat;
         }
 
-        return gasit;
+            public static ArrayList<Reportofon> cautaMemorieExterna(ArrayList<Reportofon> listaReportofon, String valoare) {
+            ArrayList<Reportofon> rezultat = new ArrayList<>();
+                    for (Reportofon a : listaReportofon) {
+                if (a.getMemorieExterna() == Integer.parseInt(valoare)){
+                    rezultat.add(a);
+                }
+            }
+            return rezultat;
+        }
+
+        public static ArrayList<Reportofon> cautaDimensiuni(ArrayList<Reportofon> listaReportofon,  String valoare) {
+            ArrayList<Reportofon> rezultat = new ArrayList<>();
+            for (Reportofon a : listaReportofon) {
+                if (a.getDimensiuni().equalsIgnoreCase(valoare)){
+                    rezultat.add(a);
+                }
+            }
+            return rezultat;
+        }
+
+        public static ArrayList<Reportofon> cautaCaliateSunet(ArrayList<Reportofon> listaReportofon,  String valoare) {
+            ArrayList<Reportofon> rezultat = new ArrayList<>();
+            for (Reportofon a : listaReportofon) {
+                if (a.getCalitateSunet().equalsIgnoreCase(valoare)){
+                    rezultat.add(a);
+                }
+            }
+            return rezultat;
+        }
+
+            public static ArrayList<Reportofon> cautaNumarMicrofoane(ArrayList<Reportofon> listaReportofon,  String valoare) {
+            ArrayList<Reportofon> rezultat = new ArrayList<>();
+            for (Reportofon a : listaReportofon) {
+                if (a.getNumarMicrofoane() == Integer.parseInt(valoare)){
+                    rezultat.add(a);
+                }
+            }
+            return rezultat;
+        }
+
+        public static ArrayList<Reportofon> cautaPret(ArrayList<Reportofon> listaReportofon,  String valoare) {
+            ArrayList<Reportofon> rezultat = new ArrayList<>();
+            for (Reportofon a : listaReportofon) {
+                if (a.getPret() == Float.parseFloat(valoare)){
+                    rezultat.add(a);
+                }
+            }
+            return rezultat;
+        }
+    public static ArrayList<Reportofon> cautaNume(ArrayList<Reportofon> listaReportofon,  String valoare) {
+        ArrayList<Reportofon> rezultat = new ArrayList<>();
+        for (Reportofon a : listaReportofon) {
+            if (a.getNume().equalsIgnoreCase(valoare)){
+                rezultat.add(a);
+            }
+        }
+        return rezultat;
     }
+    public static ArrayList<Reportofon> cautaReportofon(ArrayList<Reportofon> listaReportofon, int optiune, String valoare){
 
-    public static ArrayList<Reportofon> cautaCalitateSunetMemorieInterna(ArrayList<Reportofon> listaReportofon, String CalitateSunet, int MemorieInterna) {
-        ArrayList<Reportofon> gasit = new ArrayList<>();
-
-        for (Reportofon rep : listaReportofon) {
-            if (rep.CalitateSunet == CalitateSunet && rep.MemorieInterna == MemorieInterna) {
-                gasit.add(rep);
-            }
+        switch(optiune){
+            case 1: return cautaMemorieInterna(listaReportofon, valoare);
+            case 2: return cautaMemorieExterna(listaReportofon, valoare);
+            case 3: return cautaDimensiuni(listaReportofon, valoare);
+            case 4: return cautaCaliateSunet(listaReportofon, valoare);
+            case 5: return cautaNumarMicrofoane(listaReportofon, valoare);
+            case 6: return cautaPret(listaReportofon, valoare);
+            case 7: return cautaNume(listaReportofon, valoare);
+            default:
+                System.out.println("Optiune invalida!");
+                return new ArrayList<>();
         }
-
-        return gasit;
     }
 }
+
